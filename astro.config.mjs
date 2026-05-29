b59cb7a71b74c8d70fbda3b4a29a8952b9b27afa
XnJaca <xnjaca@gmail.com>
2026-05-29 12:24:47 -0600

    Migrar deploy de Render Node a Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://pcdmv.com',
   output: 'static',
-  adapter: node({ mode: 'standalone' }),
+  adapter: vercel(),
   build: {
     inlineStylesheets: 'auto',
   },
